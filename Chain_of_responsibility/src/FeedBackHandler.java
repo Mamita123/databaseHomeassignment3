c6c77f5a2f0c39edf53d3a24a10bd76bdcc038b2
Mamita <mamitagurung895@gmail.com>
2024-04-07 20:30:46 +0300

    Added proxy

--- Chain_of_responsibility/src/FeedBackHandler.java
+++
@@ -1,4 +0,0 @@
-public interface FeedBackHandler {
-    void handleFeedback(FeedbackMessage message);
-    void setNextHandler(FeedBackHandler handler);
-}
--- Chain_of_responsibility/src/FeedbackType.java
+++
@@ -1,6 +0,0 @@
-public enum FeedbackType {
-    compensation_claim,
-    contact_request,
-    development_suggestion,
-    general_feedback
-}
