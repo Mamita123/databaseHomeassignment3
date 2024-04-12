0d79e08e52a5accaf7d64d0e629e7e351837342a
Mamita <mamitagurung895@gmail.com>
2024-04-12 09:37:38 +0300

    Added strategy assignment

--- Adapter/src/NewDateInterface.java
+++ Adapter/src/NewDateInterface.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public interface NewDateInterface {
+public interface NewDateInterface {
+    void setDay(int day);
+    void setMonth(int month);
+    void setYear(int year);
+    int getDay();
+    int getMonth();
+    int getYear();
+    void advanceDays(int days);
 }
