c98c999ad962e0a53e5b7edd3319391ce4cf63dc
Mamita123 <112558748+Mamita123@users.noreply.github.com>
2024-04-15 11:14:11 +0300

    Delete proxy directory

--- proxy/src/main/java/org/example/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class AccessDeniedException extends RuntimeException{
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
--- proxy/src/main/java/org/example/Document.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface Document {
-    String getIdentifier();
-    String getContent();
-    String getCreationDate();
-}
