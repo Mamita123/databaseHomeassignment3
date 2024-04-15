e9f97c8d45c2d65ab95d0ef37daa342e61f08b89
Mamita123 <112558748+Mamita123@users.noreply.github.com>
2024-04-15 11:10:56 +0300

    Delete Adapter directory

--- Adapter/src/NewDateInterface.java
+++
@@ -1,9 +0,0 @@
-public interface NewDateInterface {
-    void setDay(int day);
-    void setMonth(int month);
-    void setYear(int year);
-    int getDay();
-    int getMonth();
-    int getYear();
-    void advanceDays(int days);
-}
